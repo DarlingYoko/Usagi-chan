09117fdcb490dbe64d3159a4d5fbf32e4e50766d
zartem01 <zartem01@gmail.com>
2021-01-22 01:23:41 +0100

    rewrite commads valentine, add follow for Yoko

--- config.py
+++ config.py
@@ -4,7 +4,9 @@ logChannel = 733631069542416387
 messageChannel = 734004322177646614
 serverID = 733631069542416384
 token = 'ODAxMTUzMTk3NTUyMzA0MTI5.YAciDw.Gm9x7NeXHvUrJvLfJ-tmlbD9URs'
-thumbnail = 'https://cdn.discordapp.com/attachments/801159693404864543/801271549507403826/XUKpfEBhHoc.jpg'
+thumbnail = 'https://cdn.discordapp.com/attachments/801159693404864543/801926601481912320/cbf89215894687f9f0e81aa7d125dba1_cupid-bow-arrow-hearts-cupid-heart-with-arrow-png-transparent-_860-.png'
+yokoId = 290166276796448768
+
 
 def getLogger():
     logging.config.dictConfig({
