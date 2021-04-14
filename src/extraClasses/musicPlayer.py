3cf70e15cce636d13a48d43ab1e9fd859c28983d
Yoko <zartem01@gmail.com>
2021-04-14 20:04:45 +0200

    ready mp

--- src/extraClasses/musicPlayer.py
+++ src/extraClasses/musicPlayer.py
@@ -3,7 +3,7 @@ from youtube_dl import YoutubeDL
 from gtts import gTTS
 from threading import Thread
 from random import randint
-from src.spotdl.downloadSpoti import downloadSpoti
+from src.downloadSpoti import downloadSpoti
 
 class MusicPlayer():
     def __init__(self):
