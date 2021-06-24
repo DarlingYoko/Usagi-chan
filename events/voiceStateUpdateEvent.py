e13b95c659ae2574b027a1ecf52b03279ef3541f
artem <zartem01@gmail.com>
2021-06-24 20:29:27 +0200

    refactor voiceClient functions

--- events/voiceStateUpdateEvent.py
+++ events/voiceStateUpdateEvent.py
@@ -25,28 +25,50 @@ def setVoiceStateUpdateEvent(self):
                     await discord.utils.get(self.client.voice_clients, channel=before.channel).disconnect()
                     self.musicPlayer.vc = await self.client.get_channel(after.channel.id).connect()
 
-                elif before.channel and not after.channel:
+                elif (before.channel and not after.channel and
+                        before.channel.id != self.config['data'].getint('kadrovyChannel')):
                     await discord.utils.get(self.client.voice_clients, channel=before.channel).disconnect()
                     self.musicPlayer.vc = None
 
-                elif after.channel and not before.channel:
+                elif (after.channel and not before.channel and
+                        after.channel.id != self.config['data'].getint('kadrovyChannel')):
                     self.musicPlayer.vc = await self.client.get_channel(after.channel.id).connect()
+
+
+            if before.channel and before.channel.id == self.config['data'].getint('kadrovyChannel'):
+                kadrovyChannel = await self.client.fetch_channel(self.config['data'].getint('kadrovyChannel'))
+                if len(kadrovyChannel.members) == 1:
+                    await discord.utils.get(self.client.voice_clients, channel=before.channel).disconnect()
+                    self.musicPlayer.vc = None
+
+
+            if after.channel and after.channel.id == self.config['data'].getint('kadrovyChannel'):
+                if not self.musicPlayer.vc:
+                    self.musicPlayer.vc = await self.client.get_channel(self.config['data'].getint('kadrovyChannel')).connect()
         except:
             pass
 
 
         try:
             file = None
+            usagiChannel = self.musicPlayer.vc.channel.id
             #print(before.channel and not after.channel and before.channel.id == 788865135724068904 and member.id != self.config['usersIDs'].getint('yokoId') and member.id != self.config['usersIDs'].getint('botId'))
             #print(after.channel, not before.channel, after.channel.id == 733640826256752641, member.id != self.config['usersIDs'].getint('yokoId'), member.id != self.config['usersIDs'].getint('botId'))
-            if before.channel and not after.channel and before.channel.id == 788865135724068904 and member.id != self.config['usersIDs'].getint('yokoId') and member.id != self.config['usersIDs'].getint('botId'):
-                file = '../audio/{0}_vishel.mp3'.format(member.id)
-                text = '{0} вышёл'.format(member.display_name)
+
+            if (before.channel and not after.channel and
+                member.id != self.config['usersIDs'].getint('yokoId') and
+                member.id != self.config['usersIDs'].getint('botId')):
+                if before.channel.id == usagiChannel:
+                    file = '../audio/{0}_vishel.mp3'.format(member.id)
+                    text = '{0} вышёл'.format(member.display_name)
 
 
-            elif after.channel and not before.channel and after.channel.id == 788865135724068904 and member.id != self.config['usersIDs'].getint('yokoId') and member.id != self.config['usersIDs'].getint('botId'):
-                file = '../audio/{0}_voshel.mp3'.format(member.id)
-                text = '{0} зашёл'.format(member.display_name)
+            elif (after.channel and not before.channel and
+                member.id != self.config['usersIDs'].getint('yokoId') and
+                member.id != self.config['usersIDs'].getint('botId')):
+                if after.channel.id == usagiChannel:
+                    file = '../audio/{0}_voshel.mp3'.format(member.id)
+                    text = '{0} зашёл'.format(member.display_name)
 
             if file:
                 language = 'ru'
--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 
-
-def asd():
-    print('asd')
-
-
-a = asd
-
+if (1010100101 == 10120021020 and
+    230498 == 2341234):
+    print(1)
 
 
 # https://github.com/heroku/heroku-buildpack-chromedriver
