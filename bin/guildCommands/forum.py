cafec9f2db1395604654c1c18b60f342f3a90ffc
artem <zartem01@gmail.com>
2021-07-07 19:03:03 +0200

    fix

--- bin/guildCommands/forum.py
+++ bin/guildCommands/forum.py
@@ -4,6 +4,7 @@ from time import mktime
 async def forum(self):
     if datetime.now().hour == 16:
         channel = await self.client.fetch_channel(858053937008214018)
+        await channel.send('Не забываем забрать логин бонус!\nhttps://webstatic-sea.mihoyo.com/ys/event/signin-sea/index.html?act_id=e202102251931481&lang=ru-ru')
         channel = await self.client.fetch_channel(858093764408508436)
         await channel.send('Не забываем забрать логин бонус!\nhttps://webstatic-sea.mihoyo.com/ys/event/signin-sea/index.html?act_id=e202102251931481&lang=ru-ru')
 
