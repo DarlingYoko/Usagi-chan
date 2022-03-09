cd87132c0a795fca0132eec1ded0dfb77f17d322
Artem Zaitsev <zartem01@gmail.com>
2022-03-09 21:52:38 +0100

    fix

--- cogs/beer/extra.py
+++ cogs/beer/extra.py
@@ -3,12 +3,13 @@ async def buy_based_apple(ctx):
     return 'БАХНУЛ БАЗОВОГО ЯБЛОЧКА НА ТРОИХ И СИДИТ КАЙФУЕТ'
 
 async def send_wesdos_nahui(ctx):
-    member = await ctx.guild.fetch_member(332882488961662978)
-    channel = await ctx.guild.fetch_channel(858053937008214018)
+    guild = await ctx.guild.fetch_guild(858053936313008129)
+    member = await guild.fetch_member(332882488961662978)
+    channel = await guild.fetch_channel(858053937008214018)
     await channel.send(f'{member.mention}, ИДИ НАХУЙ!')
     return 'Послал Весдоса нахуй'
 
-    
+
 
 async def buy_server_boost(ctx):
     return 'Жесть ты кадр конечно, гц гц, <@290166276796448768> у нас тут пчел нарисовался, бусты купить хочет'
