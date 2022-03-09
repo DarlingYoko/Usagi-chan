4213637fb40017931814ae6b48473b5e24514272
Artem Zaitsev <zartem01@gmail.com>
2022-03-09 21:49:11 +0100

    invisible nahui

--- cogs/beer/extra.py
+++ cogs/beer/extra.py
@@ -4,8 +4,11 @@ async def buy_based_apple(ctx):
 
 async def send_wesdos_nahui(ctx):
     member = await ctx.guild.fetch_member(332882488961662978)
-    await ctx.send(f'{member.mention}, ИДИ НАХУЙ!')
+    channel = await ctx.guild.fetch_channel(858053937008214018)
+    await channel.send(f'{member.mention}, ИДИ НАХУЙ!')
     return 'Послал Весдоса нахуй'
+
+    
 
 async def buy_server_boost(ctx):
     return 'Жесть ты кадр конечно, гц гц, <@290166276796448768> у нас тут пчел нарисовался, бусты купить хочет'
